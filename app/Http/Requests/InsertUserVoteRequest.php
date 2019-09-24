7508854b08f8ff57f3ab0624d6276fd26ad3ac50
mohamamad <mohamad.shahrokhi@gmail.com>
2019-09-24 10:01:09 +0330

    user vote bug fix

--- app/Http/Requests/InsertUserVoteRequest.php
+++ app/Http/Requests/InsertUserVoteRequest.php
@@ -2,16 +2,11 @@
 
 namespace App\Http\Requests;
 
-use App\Rules\Active;
-use App\Rules\Enable;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Http\Request;
-use Illuminate\Validation\Rule;
 
 class InsertUserVoteRequest extends FormRequest
 {
-    private $voteID;
-
     /**
      * Determine if the user is authorized to make this request.
      *
@@ -31,20 +26,7 @@ class InsertUserVoteRequest extends FormRequest
     public function rules(Request $request)
     {
         return [
-            'user_id'    => ['required','min:1' , Rule::unique('user_vote_option')->where(function ($query)  {
-                return $query->where('vote_id', $this->voteID );
-            })],
-            'vote_id'    => ['required' , new Active],
-            'option_id'  => ['required' , new Enable , Rule::exists('options','id')->where('vote_id', $this->voteID)],
+            'option_id'  => ['required'],
         ];
-    }
-
-    public function prepareForValidation()
-    {
-        $input = $this->request->all();
-        $this->voteID = $input['vote_id'];
-        $input['user_id'] = request()->user()->id;
-        $this->replace($input);
-        parent::prepareForValidation();
     }
 }
